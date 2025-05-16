662bf1e8450b1c94cbcddcfc473e9007dd380086
Sunny Sahu <sunnysahu8210@gmail.com>
2025-05-16 19:09:24 +0530

    Connection Succesful

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Data.SqlClient;
 
 public class Customer
 {
-    public static void main(string args[])
+    public static void Main(String[] args)
     {
+        //string myConnection = "Data Source=MSI;Database=ADOTEST; Initial Catalog=sspi";
+        string myConnection = "Data Source=MSIS;Initial Catalog=ADOTEST;Integrated Security=True;";
 
+
+        try
+        {
+            SqlConnection connection = new SqlConnection(myConnection);
+
+            connection.Open();
+
+            Console.WriteLine("Connection Successful");
+
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Connection Unsuccessful" + ex.Message);
+        }
+        
     }
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+﻿1. Create an Connection String for establishing connection to the database
+	> Create a Database in SQL and use the Server name and Database name to create a connection string
+1. 
+2. Setup it Here
+	Here `MSI` is SQL server name and `ADOTEST`
+	```
+	   string myConnection = "Data Source=MSI;Database=ADOTEST; Initial Catalog=sspi";
+        string myConnection = "Data Source=MSI;Initial Catalog=ADOTEST;Integrated Security=True;";
+	```
